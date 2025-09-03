5c0fc3111d47fd28d80e6b18688841d8918e646a
hashif228 <hashifs228@gmail.com>
2025-09-03 22:57:47 +0530

    vercel.json add

--- Ftnd_react/src/components/ProtectedRoute.jsx
+++ Ftnd_react/src/components/ProtectedRoute.jsx
@@ -1,12 +1,10 @@
-
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("access_token");
 
   if (!token) {
-    // No token → redirect to login
-    return <Navigate to="/" replace />;
+    return <Navigate to="https://crmadmin-blush.vercel.app/" replace />;
   }
 
   return children;
