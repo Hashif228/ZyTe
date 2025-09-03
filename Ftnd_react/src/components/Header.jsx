ca43ce51b1cbeaaa561e0c7b34648ec08b3b473d
hashif228 <hashifs228@gmail.com>
2025-09-03 23:51:43 +0530

    logout fuction added

--- Ftnd_react/src/components/Header.jsx
+++ Ftnd_react/src/components/Header.jsx
@@ -1,16 +1,21 @@
 
 import styles from './Header.module.css';
 import { useState,useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function Header({title}){
     const [username, setUsername] = useState("");
-
+    const navigate=useNavigate()
   useEffect(() => {
     const storedUsername = localStorage.getItem("username");
     if (storedUsername) {
       setUsername(storedUsername);
     }
   }, []);
+  const handleLogout = () => {
+    localStorage.clear(); 
+    navigate("/"); 
+  };
     return(
                  <div className={styles.header}>
                     <div className={styles.headerContent}>
@@ -19,7 +24,7 @@ export default function Header({title}){
                                 <div className={styles.pageTitle}>{title}</div>
                         </div>
                         <div className={styles.searchContainer}>
-                        <div className={styles.logout}>logout</div>
+                        <div className={styles.logout} onClick={handleLogout}>logout</div>
                             <div className={styles.searchInput}>
                                 <div className={styles.searchInputChild} ></div>
                                 <div className={styles.component1}>
