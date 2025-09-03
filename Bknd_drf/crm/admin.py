6c4f35436fd085ca82ae32d277e4c470717eefa4
hashif228 <hashifs228@gmail.com>
2025-09-03 15:48:45 +0530

    admin removed

--- Bknd_drf/crm/admin.py
+++ Bknd_drf/crm/admin.py
@@ -4,7 +4,7 @@ from .models import Customer,Staff,Department,Manager
 
 
 
-admin.site.register(AdminUser)
+# admin.site.register(AdminUser)
 admin.site.register(Department)
 admin.site.register(Manager)
 admin.site.register(Staff)
