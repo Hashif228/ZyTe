24531c7f13d47babab642d6aa9b4660dc10f0e0f
hashif228 <hashifs228@gmail.com>
2025-09-04 08:08:14 +0530

    posgre db added

--- Bknd_drf/crm_admin/settings.py
+++ Bknd_drf/crm_admin/settings.py
@@ -79,11 +79,13 @@ WSGI_APPLICATION = 'crm_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
 }
 
 
